830bb8984ac42848d1b1de58a06665169fe3b956
marcio <daniel.okawa@gmail.com>
2019-03-29 20:35:09 -0300

    timeline line

---
+++ src/background/background.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './background.css'
+
+class Background extends Component {
+	render() {
+		return(
+			<div>
+				<div class='background'/> 
+				<div class="outer">
+				  	<div class="middle">
+				    	<div class="inner">
+				    		<h1 class="name">Daniel Akio Okawa</h1>
+				      		<h1 class="subtitle">ANALISTA DE PESQUISA E INOVAÇÃO</h1>
+				    	</div>
+				  	</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Background;
