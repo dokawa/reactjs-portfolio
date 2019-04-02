a4ef344816a8656ea684a11107825023e0f341cf
marcio <daniel.okawa@gmail.com>
2019-04-01 22:19:32 -0300

    filled experience

---
+++ src/footer/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './footer.css';
+
+class Footer extends Component{
+	render() {
+		return (
+			<div class="footer">© 2019 por Daniel Akio Okawa</div>
+		);
+	}
+}
+
+export default Footer;
---
+++ src/personal-projects/personal-projects.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Timeline from '../timeline/timeline'
+
+
+class PersonalProjects extends Component {
+	constructor(props) {
+  		super(props);
+
+  		this.state = { entries: [
+  			{ year: "2018", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+				    Nulla et nulla non velit sagittis molestie sed a tellus. \
+				    Etiam dui odio, tristique ac ornare id, dignissim et ex. \
+				    Etiam mattis consequat ultrices. Curabitur lorem purus, \
+				    faucibus sit amet faucibus at, aliquet ut leo. Fusce tempus mi vel nibh interdum,  \
+	    			vitae vulputate mi accumsan. Nunc scelerisque ut nulla quis porta. In porttitor erat eget lacinia porta. " },
+	    	{ year: "2019", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+				    Nulla et nulla non velit sagittis molestie sed a tellus. \
+				    Etiam dui odio, tristique ac ornare id, dignissim et ex. \
+				    Etiam mattis consequat ultrices. Curabitur lorem purus, \
+				    faucibus sit amet faucibus at, aliquet ut leo. Fusce tempus mi vel nibh interdum,  \
+	    			vitae vulputate mi accumsan. Nunc scelerisque ut nulla quis porta. In porttitor erat eget lacinia porta. "  },
+	    	{ year: "2020", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+				    Nulla et nulla non velit sagittis molestie sed a tellus. \
+				    Etiam dui odio, tristique ac ornare id, dignissim et ex. \
+				    Etiam mattis consequat ultrices. Curabitur lorem purus, \
+				    faucibus sit amet faucibus at, aliquet ut leo. Fusce tempus mi vel nibh interdum,  \
+	    			vitae vulputate mi accumsan. Nunc scelerisque ut nulla quis porta. In porttitor erat eget lacinia porta. " }
+
+  		] };
+  	}
+
+
+	render() {
+		return(
+			<Timeline entries = { this.state.entries }/>
+		);
+	}
+
+}
+
+export default PersonalProjects;
