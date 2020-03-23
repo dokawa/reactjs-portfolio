3db86001bf1392f6bde3c8058c7654fa50a878cf
dokawa <daniel.okawa@gmail.com>
2020-03-23 14:42:05 -0300

    added logos

--- src/hobbies-and-interests/investing/investing.js
+++ src/hobbies-and-interests/investing/investing.js
@@ -6,7 +6,7 @@ class Investing extends Component {
 	render() {
 		return (
 			<div class='investing-container'>
-				<div><a href="https://www.easynvest.com.br/"><img class="logo-icon" src= { require('assets/images/hobbies-and-interests/investing/easynvest-logo.svg') } /></a></div>
+				<div><a href="https://www.easynvest.com.br/"><img class="logo-icon" src= { require('assets/images/hobbies-and-interests/investing/easynvest-logo.png') } /></a></div>
 				<div><a href="https://www.clear.com.br/site"><img class="logo-icon" src= { require('assets/images/hobbies-and-interests/investing/clear-logo.png') } /></a></div>
 				<div><a href="https://www.clear.com.br/site"><img class="logo-icon" src= { require('assets/images/hobbies-and-interests/investing/rico-logo.png') } /></a></div>
 				<div><a href="https://www.binance.com/?ref=11130872"><img class="logo-icon" src= { require('assets/images/hobbies-and-interests/investing/binance-logo.png') } /></a></div>
